ba219c8b82e005f8a2de43b6c44517bafe365426
Jishnulal <jishnulal9497@gmail.com>
2023-03-20 23:45:18 +0530

    Update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,21 +1,27 @@
+import 'package:catalog_app/model/catalog.dart';
 import 'package:catalog_app/widgets/drawer.dart';
+import 'package:catalog_app/widgets/items.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
-  
+
   String days = 'Jishnu';
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Catalouge App'),
+        title: const Text('Catalog App'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-          child: Text('Hi $days'),
-        ),
+      body: ListView.builder(
+        itemCount: dummyList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ItemLog(
+            item: dummyList[index],
+          );
+        },
       ),
       drawer: const MyDrawer(),
     );
